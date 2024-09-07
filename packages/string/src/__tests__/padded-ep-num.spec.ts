f48557a941f8a9e826d28f97980413644c2ac033
Joshua Avalon <git@avalon.sh>
2024-09-07 17:24:01 +0800

    chore: fix test

--- packages/string/src/__tests__/padded-ep-num.spec.ts
+++ packages/string/src/__tests__/padded-ep-num.spec.ts
@@ -3,7 +3,7 @@ import { paddedEpNum } from "../padded-ep-num.js";
 
 describe("Test paddedUrl", async () => {
   it("should pad", async () => {
-    const url = await paddedEpNum`https://example.com/${2}.html`({ ep: { num: 1 } } as any);
+    const url = await paddedEpNum`https://example.com/${2}.html`({ epInfo: { num: 1 } } as any);
     assert.equal(url, "https://example.com/01.html");
   });
 });
